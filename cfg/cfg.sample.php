04d2b82001e095b883fd258554880b198e7f4a30
daiko <daiko@daiko.fr>
2017-12-01 16:23:37 +0100

    Ignore seulement le fichier de configuration

--- cfg/cfg.sample.php
+++ cfg/cfg.sample.php
@@ -1,6 +1,5 @@
 <?php
-	$db_server   = '';
-	$db_user     = '';
-	$db_password = '';
-	$db_base     = $db_user;
-?> 
+    $db_server = '';
+    $db_user = '';
+    $db_password = '';
+    $db_base = $db_user;
